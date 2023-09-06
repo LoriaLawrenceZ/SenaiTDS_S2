7768d17ac7c2a3b3155ad0fd6c2768f14c9c7144
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2023-09-06 13:57:26 -0300

    GUI Layouts

---
+++ Java/GUI/GUILayout/src/ExemploFlowLayout.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+
+public class ExemploFlowLayout {
+    public ExemploFlowLayout() {
+        JFrame janela = new JFrame("Janela JFrame");
+
+        // Mudar layout Padrão (Grid) -> Flow
+    }
+}
