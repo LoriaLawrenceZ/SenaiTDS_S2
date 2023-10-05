561ef560df66e4f6fba734196259e60571a1d538
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2023-10-05 14:48:10 -0300

    Learning Event Handling

--- Java/GUI/TratamentoDeEvento/src/ExemploNomeSobrenome.java
+++ Java/GUI/TratamentoDeEvento/src/ExemploNomeSobrenome.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 
 public class ExemploNomeSobrenome extends JFrame {
     // Criando Componentes
-    private ArrayList<JComponent> componentes = new ArrayList<JComponent>(){
+    private ArrayList<JComponent> componentes = new ArrayList<JComponent>() {
         {
             // Primeira Linha (nome: )
             add(new JLabel("Nome: "));
@@ -28,67 +28,72 @@ public class ExemploNomeSobrenome extends JFrame {
         }
     };
 
-    //Construtor
+    // Construtor
     public ExemploNomeSobrenome() {
         super("Exemplo Nome Sobrenome");
         this.setDefaultCloseOperation(2);
         this.setBounds(100, 100, 300, 150);
-        
-        //Setando layout para o Painel Principal
+        this.setVisible(true);
+
+        // Setando layout para o Painel Principal
         JPanel mainPanel = new JPanel(new GridLayout(3, 2));
-        this.add(mainPanel); //Adicionando mainPanel ao Frame
+        this.add(mainPanel); // Adicionando mainPanel ao Frame
 
         for (int i = 0; i < componentes.size(); i++) {
             mainPanel.add(componentes.get(i)); // Adicionando componente ao Painel Principal
 
-            if(i == 4){
+            if (i == 4) {
                 // Chamando os componentes necessários
                 // JTextField nomeTextField = (JTextField) componentes.get(1); // Campo Nome
-                // JTextField sobrenomeTextField = (JTextField) componentes.get(3); // Campo Sobrenome
-                JButton botao = (JButton) componentes.get(4); //Botão
+                // JTextField sobrenomeTextField = (JTextField) componentes.get(3); // Campo
+                // Sobrenome
+                JButton botao = (JButton) componentes.get(4); // Botão
                 // JLabel resultado = (JLabel) componentes.get(5); //Resultado
 
-                //-----=== 3 Formas de Tratar Eventos ===-----//
+                // -----=== 3 Formas de Tratar Eventos ===-----//
                 /*
-                * 1° - Callback (só funciona nos Tratamentos de Evento que contém APENAS 1 MÉTODO)
-                * 
-                * 2° - Chamando a Classe ["new ActionListener" -> actionPerformed(ActionEvent)]
-                *
-                * 3° - Handler
-                */
+                 * 1° - Callback (só funciona nos Tratamentos de Evento que contém APENAS 1
+                 * MÉTODO)
+                 * 
+                 * 2° - Chamando a Classe ["new ActionListener" -> actionPerformed(ActionEvent)]
+                 *
+                 * 3° - Handler
+                 */
 
-                //---==Exemplo CALLBACK==---//
+                // ---==Exemplo CALLBACK==---//
                 /*
-                * botao.addActionListener( e -> { //Listener
-                *     //Evento
-                *     resultado.setText(nomeTextField.getText() +" "+ sobrenomeTextField.getText()); //Exibindo o nome completo
-                *
-                *     // Limpando os campos
-                *     nomeTextField.setText("");
-                *     sobrenomeTextField.setText("");
-                * });
-                */
+                 * botao.addActionListener( e -> { //Listener
+                 * //Evento
+                 * resultado.setText(nomeTextField.getText() +" "+
+                 * sobrenomeTextField.getText()); //Exibindo o nome completo
+                 *
+                 * // Limpando os campos
+                 * nomeTextField.setText("");
+                 * sobrenomeTextField.setText("");
+                 * });
+                 */
 
-                //---==Exemplo 2° FORMA==---//
+                // ---==Exemplo 2° FORMA==---//
                 /*
-                * 
-                * botao.addActionListener( new ActionListener(){ //Listener
-                *     public void actionPerformed(ActionEvent e){
-                *         //Evento
-                *         resultado.setText(nomeTextField.getText() +" "+ sobrenomeTextField.getText()); //Exibindo o nome completo
-                *
-                *         // Limpando os campos
-                *         nomeTextField.setText("");
-                *         sobrenomeTextField.setText("");
-                *     }
-                * });
-                */
+                 * 
+                 * botao.addActionListener( new ActionListener(){ //Listener
+                 * public void actionPerformed(ActionEvent e){
+                 * //Evento
+                 * resultado.setText(nomeTextField.getText() +" "+
+                 * sobrenomeTextField.getText()); //Exibindo o nome completo
+                 *
+                 * // Limpando os campos
+                 * nomeTextField.setText("");
+                 * sobrenomeTextField.setText("");
+                 * }
+                 * });
+                 */
 
-                //---==Exemplo HANDLER==---//
+                // ---==Exemplo HANDLER==---//
                 /*
                  * Handler handler = new Handler();
                  * botao.addActionListener(handler);
-                */
+                 */
                 Handler handler = new Handler();
                 botao.addActionListener(handler);
             }
@@ -101,10 +106,10 @@ public class ExemploNomeSobrenome extends JFrame {
         public void actionPerformed(ActionEvent e) {
             JTextField nomeTextField = (JTextField) componentes.get(1); // Campo Nome
             JTextField sobrenomeTextField = (JTextField) componentes.get(3); // Campo Sobrenome
-            JLabel resultado = (JLabel) componentes.get(5); //Resultado
+            JLabel resultado = (JLabel) componentes.get(5); // Resultado
 
-            //Evento
-            resultado.setText(nomeTextField.getText() +" "+ sobrenomeTextField.getText()); //Exibindo o nome completo
+            // Evento
+            resultado.setText(nomeTextField.getText() + " " + sobrenomeTextField.getText()); // Exibindo o nome completo
 
             // Limpando os campos
             nomeTextField.setText("");
