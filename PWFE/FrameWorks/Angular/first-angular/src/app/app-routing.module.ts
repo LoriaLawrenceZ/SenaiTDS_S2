89ea65ed3b971aefe47574ee70d4dd33a6336d98
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2023-10-06 17:00:34 -0300

    Learning Angular

--- PWFE/FrameWorks/Angular/first-angular/src/app/app-routing.module.ts
+++ PWFE/FrameWorks/Angular/first-angular/src/app/app-routing.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { ContatoComponent } from './contato/contato.component';
 
 const routes: Routes = [
   { path: 'header', component: HeaderComponent },
   { path: 'footer', component: FooterComponent },
+  { path: '', component: HomeComponent },
+  { path: 'sobre', component: SobreComponent },
+  { path: 'contato', component: ContatoComponent },
+
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
 @NgModule({
--- PWFE/FrameWorks/Angular/first-angular/src/app/app.module.ts
+++ PWFE/FrameWorks/Angular/first-angular/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { ContatoComponent } from './contato/contato.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    SobreComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
