3f322991f0510b2e9fe733669e438d1a15ddabdd
Lorenzo O. Zimbres <97912499+LoriaLawrenceZ@users.noreply.github.com>
2023-10-09 19:46:32 +0000

    Fazendo exercício de operações matemáticas

---
+++ PWBE/ConexaoMySQL/listCadastro.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once(conexao.php);
+
+//Código SQL
+$sql_code = "SELECT * FROM cadastros;";
+//Executando o código sql no banco de dados
+$sql_query = $mysqli->query($sql_code) or die("Falha na execução do códigomSQL: ".$mysqli->error);
+
+// Fazendo if para evitar erros (como array não inicializado)
+if($sql_query->num_rows == 0){
+    $pedidos = null
+}
+else{
+    while($pedido = $sql_query->fetch_assoc()){
+        $pedidos[] = $pedido;
+    }
+}
+
+?>
