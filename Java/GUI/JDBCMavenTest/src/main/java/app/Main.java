a038d94959f417ae267f721170c4b5bcb19edda0
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2023-10-31 15:12:11 -0300

    Learning JDBC with Maven

--- Java/GUI/JDBCMavenTest/src/main/java/app/Main.java
+++ Java/GUI/JDBCMavenTest/src/main/java/app/Main.java
@@ -1,14 +1,15 @@
+package app;
+
+import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.Statement;
 
-public class App {
-    public static void main(String[] args) throws Exception {
+public class Main {
+    public static void main(String[] args) {
         try {
             Statement s; // Cria uma instância de Statement para executar comandos SQL.
             String usuario = "postgres"; // Nome de usuário do banco de dados.
             String senha = "postgres"; // Senha do banco de dados.
-            //tipo de conexão | tipo do BD |   endereço   | BD
             String url = "jdbc:postgresql://localhost:5432/postgres"; // URL de conexão com o banco de dados PostgreSQL.
 
             // Estabelece a conexão com o banco de dados.
@@ -16,7 +17,7 @@ public class App {
             s = c.createStatement(); // Cria um objeto Statement a partir da conexão.
             // Executa uma codigo SQL de inserção na tabela "contato".
             // ser substituídos pelos valores reais a serem inseridos.
-            s.execute("INSERT INTO contato VALUES('2', 'Fulano', 'fulano@gmail.com')");
+            s.execute("DELETE FROM contato WHERE id=1;");
             c.close(); // Fecha a conexão com o banco de dados após a conclusão da operação.
         } catch (Exception E) {
             System.out.println("\n" + E); // Captura e imprime qualquer exceção que ocorra durante a execução.
