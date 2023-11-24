d75555cc4aab195f5d8c8f1eac5e5435dffbd13e
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2023-11-24 14:01:55 -0300

    API mapa ok

---
+++ PWFE/FrameWorks/React/react-localizacao-em-tempo-real/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+import MapaAtual from "./Mapa/MapaAtual";
+// Define o componente principal da aplicação, chamado 'App'.
+function App() {
+  return (
+    <div className="App">
+      <MapaAtual />
+    </div>
+  );
+}
+// Exporta o componente 'App' como o componente principal da aplicação.
+export default App;
